cc2b5843d4bc49fedd062667eca859092b100261
Purna <purnamang126@gmail.com>
2022-11-02 23:21:07 -0600

    Added Comments

--- artist.py
+++ artist.py
@@ -13,57 +13,63 @@ def connect(path):
     return
 
 def addSong(aid):
+    # The artists adds a song to the database by providing a title, a duration and the performing artist and adding a unique song id to it.
+    # It also prevents adding duplicate songs with same name and duration. 
     global connection, cursor
     title= map(string, input("Enter the song title: "))
     while title:
     #loops till a positive integer is entered after the title is entered
         try:
             duration = int(input("Enter the song duration: "))
-            assert duration > 0
+            assert duration > 0 
         except AssertionError:
             print("Error! Please input a positive number.")
+            #Error if a negative no. or 0 is entered
             continue
         break
-    artists = list(map(str, input("Enter the ids of additional artists (separated by space). If none, enter space: ").split()))
-    if aid not in artists:
+    artists = list(map(str, input("Enter the ids of artists (separated by space) performing this song. If none, enter space: ").split()))
+    if aid not in artists: #if user forgets to input their aid to add to the artists
         artists.append(aid) #Adding the current artist aid to the list of all artist performing the song
         
-    cursor.execute('''SELECT aid FROM artists''')
-    artist_aid= cursor.fetchall()
+    cursor.execute('''SELECT aid FROM artists''') 
+    artist_aid= cursor.fetchall() 
     for i in artists:
         if i not in artist_aid:
+            # to check if all the entered aids are there in database
             print("Error! The artist aid" + i + "does not exist")
-            addSong(aid)
+            addSong(aid) #Going back to add song again
             return
 
     checkSong = ('''SELECT * 
                   FROM songs s
                   WHERE s.title LIKE ?
                   AND s.duration = ?''')
+    #Query to check if any song with same title and duration exists in the database
     songExist = cursor.execute(checkSong, (title, duration))
     songExist = cursor.fetchone()
     cursor.execute('''SELECT MAX(s.sid) 
                     FROM songs s;''')
-    newsid = cursor.fetchall()[0][0] + 1
+    newsid = cursor.fetchall()[0][0] + 1 #Unique sid to add new song
     if (songExist):
         print("This song already exists in the database")
-        artistAction(aid)
+        artistAction(aid) 
     else:
-        print("continue to add a song")
+        print("Adding the song")
         cursor.execute('''INSERT INTO songs VALUES(?, ?, ?)''', (newsid, title, duration))
         cursor.commit()
-    
+    print("Adding all the performers of the song")
     for i in artists:
         cursor.execute('INSERT INTO perform VALUES(?, ?)', (i, newsid))
         connection.commit()
-    ##adding additional performers
-    
+    ##adding additional performers 
+    artistAction(aid) #Going back to artist action
     return
 
 def topFans(aid):
+    # This function finds the top 3 users who listen to this artist's songs (aid taken) the longest time
     global connection, cursor
     print("Top 3 Fans are:\n")
-    cursor.execute('''SELECT u.name, SUM(l.cnt * s.duration) 
+    cursor.execute('''SELECT u.uid, u.name, SUM(l.cnt * s.duration) 
                     FROM listen l, perform p, user u, songs s, artists a
                     WHERE p.aid= ?
                     AND l.sid = s.sid 
@@ -79,9 +85,10 @@ def topFans(aid):
     return
 
 def topPlaylist(aid):
+    #This function finds the top 3 playlist that has the largest number of songs of this artist(aid taken).
     global connection, cursor
     print("Top 3 Playlists are:\n")
-    cursor.execute('''SELECT pli.pid, pli.title, COUNT(*)
+    cursor.execute('''SELECT pli.pid, pl.title, COUNT(*)
                     FROM perform p, plinclude pli, playlists pl
                     WHERE p.aid = ?
                     AND p.sid = pli.sid
@@ -95,6 +102,7 @@ def topPlaylist(aid):
     return
 
 def artistAction(aid):
+    # This function displays all action that can be performed on a song by a artist and also provides option to logout or quit the program.
     print("Please select a number between 1 to 4 as desceibed below):\n ")
     print("1. Add a song \n2. Find top fans and playlists \n3. Log out \n4. Quit the program")
     cmd = int(input())
