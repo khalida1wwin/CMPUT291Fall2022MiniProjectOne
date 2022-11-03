8389d89cee0a1ee99a98678af81c20c8968cfe00
Purna <purnamang126@gmail.com>
2022-11-02 21:04:50 -0600

    Working on add to playlist

--- songactions.py
+++ songactions.py
@@ -4,18 +4,87 @@ import string
 connection = None
 cursor = None
 
-def info(aid_from_songs):
+def info(sid,uid):
+    # More information for a song is the names of artists who performed it 
+    # in addition to id, title and duration of the song as well as the names of 
+    # playlists the song is in (if any).
     global connection, cursor
-    cursor.execute(''' SELECT s.sid, s.title, s.duration 
-                            FROM artists a, songs s, perform p
-                            WHERE a.aid = :artist_num
-                            AND a.aid = p.aid
-                            AND p.sid = s.sid
-                            GROUP BY s.sid
-                        ''', {'artist_num': aid_from_songs})
-    artist_info = cursor.fetchall()
+    print("Song Details\n")
+    cursor.execute('''SELECT sid, title, duration 
+                    FROM songs 
+                    WHERE sid = ?''', sid)
+    all_entry = cursor.fetchall()
+    for one_entry in all_entry:
+        print(one_entry)
+    print("Artist Details\n")
+    cursor.execute('''SELECT a.name
+                    FROM perform p, artists a
+                    WHERE p.sid = ?
+                    AND a.aid = p.aid''', sid)
+    artists = cursor.fetchall()
+    for one_entry in artists:
+        print(one_entry)
+    print("Playlist Details\n")
+    cursor.execute('''SELECT p.title 
+                    FROM playlists p, plinclude pl
+                    WHERE pl.pid = p.pid 
+                    AND pl.sid = ?''', sid)
+    for one_entry in artists:
+        print(one_entry)
+    songAction(sid,uid) # Go back to the menu after everything is done??
     return
-def songAction(song):
+
+def listen(sid, uid):
+    global connection, cursor
+    checkSession = '''SELECT sno FROM sessions
+                    WHERE uid LIKE ? 
+                    AND end IS NULL;'''
+    sessionExist = cursor.execute(checkSession, uid)
+    sessionExist = cursor.fetchone()
+    if(len(sessionExist)==0):
+        sno = startSession(uid) 
+        #Assumed that the session number is returned 
+    else:
+        sno = checkSession[0]
+        checkSong = '''SELECT *
+                       FROM listen 
+                       WHERE uid LIKE ?
+                       AND sid = ?
+                       AND sno = ?'''
+        songExist = cursor.execute(checkSong, (uid,sid,sno))
+        songExist = cursor.fetchone()
+        #You can assume the user cannot have more than one active session.
+        if len(songExist)!=0:
+            cursor.execute= ('''UPDATE listen 
+                            SET cnt=cnt+1 
+                            WHERE uid=? 
+                            AND sid=? 
+                            AND sno=?''', (uid,sid,sno))
+            connection.commit()
+        else:
+            cursor.execute('''INSERT INTO listen VALUES (?, ?, ?, 1)''', (uid,sid,sno))
+            connection.commit()
+    print("Listening to Song")
+    songAction(sid,uid)
+    return
+def addToPL(sid, uid):
+    playlist = input("Enter the name of the plylist you wan to add to")
+    # Forum: Yes, sid and pid are keys for songs and playlists but the titles can be the same.
+    checkSong = '''SELECT pid FROM playlists
+                    WHERE title LIKE ? 
+                    AND uid = ?;'''
+    songExist = cursor.execute(checkSong, (playlist,uid))
+    songExist = cursor.fetchall()
+    if len(songExist)!=0:
+        pid = songExist[0][0]
+        cursor.execute('''SELECT MAX(sorder) 
+                        FROM plinclude 
+                        WHERE pid= ?;''',pid)
+        sorder = cursor.fetchall()[0][0] + 1
+        
+
+    return
+def songAction(sid, uid):
     print("Please select a number between 1 to 6 as desceibed below):\n ")
     print('1. Listen to this song.\n')
     print('2. More information.\n')
@@ -25,13 +94,13 @@ def songAction(song):
     print('6.Exit\n')
     cmd = int(input())
     if cmd == '1':
-        listenSong(song) 
+        listen(sid,uid) 
         return
     elif cmd == '2':
-        info(aid_from_songs)
+        info(sid,uid)
         return
     elif cmd == '3':
-        addtoPL()
+        addToPL(sid,uid)
         return
     elif cmd == '4':
         menu()
